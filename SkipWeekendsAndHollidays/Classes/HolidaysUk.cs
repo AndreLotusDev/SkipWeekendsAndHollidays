fa467a9077e4aed805659ddeb3f36bc9df1b7f0d
Andre Soares Gomes <andrsoares953@yahoo.com>
2023-02-07 20:52:21 -0300

    Add project files.

---
+++ SkipWeekendsAndHollidays/Classes/HolidaysUk.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+
+namespace SkipWeekendsAndHollidays.Classes
+{
+    public class EnglandAndWales
+    {
+        public string division { get; set; }
+        public List<Event> events { get; set; } = new();
+    }
+
+    public class Event
+    {
+        public string title { get; set; }
+        public string date { get; set; }
+        public string notes { get; set; }
+        public bool bunting { get; set; }
+    }
+
+    public class NorthernIreland
+    {
+        public string division { get; set; }
+        public List<Event> events { get; set; } = new();
+    }
+
+    public class HolidaysUk
+    {
+        [JsonProperty("england-and-wales")]
+        public EnglandAndWales englandandwales { get; set; }
+        public Scotland scotland { get; set; }
+
+        [JsonProperty("northern-ireland")]
+        public NorthernIreland northernireland { get; set; }
+    }
+
+    public class Scotland
+    {
+        public string division { get; set; }
+        public List<Event> events { get; set; } = new();
+    }
+}
---
+++ SkipWeekendsAndHollidays/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Net;
+using Newtonsoft.Json;
+using SkipWeekendsAndHollidays.Classes;
+
+List<Event> hollidays = new();
+
+var todayDate = DateTime.Now.Date;
+
+if (todayDate.DayOfWeek == DayOfWeek.Saturday || todayDate.DayOfWeek == DayOfWeek.Sunday)
+{
+    Console.WriteLine("Today is a weekend");
+}
+
+string jsonString = string.Empty;
+string url = @"https://www.gov.uk/bank-holidays.json";
+
+HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+
+using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+using (Stream stream = response.GetResponseStream())
+using (StreamReader reader = new StreamReader(stream))
+{
+    jsonString = reader.ReadToEnd();
+    var bankHolidays = JsonConvert.DeserializeObject<HolidaysUk>(jsonString);
+    hollidays = bankHolidays
+        .englandandwales.events
+        .Concat(bankHolidays.scotland.events)
+        .Concat(bankHolidays.northernireland.events)
+        .ToList();
+}
+
+if (hollidays.Any(a => DateTime.Parse(a.date).Date == todayDate))
+{
+    Console.WriteLine("Today is a holliday");
+}
